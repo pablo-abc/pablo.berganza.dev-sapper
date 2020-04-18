060e122b1a849c974c98853f745a21bc4c6874c7
Pablo Berganza <pberganza10@gmail.com>
2020-04-17 22:03:13 -0600

    :sparkles: Add 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,39 @@
+<section class=not-found>
+  <header>
+    <h1>404</h1>
+    <section class=message>
+      <h3>It seems you're lost.</h3>
+      <h3>Maybe try some of the links above?</h3>
+      <hr>
+      <h3>Parece que estás perdido.</h3>
+      <h3>Tal vez intenta seguir con uno de los enlaces arriba?</h3>
+    </section>
+  </header>
+</section>
+
+<style>
+  hr {
+      color: var(--color-primary-2);
+  }
+
+  .not-found {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+  }
+
+  h1 {
+      color: var(--color-primary-1);
+      font-size: 200px;
+      margin: 0;
+  }
+
+  h3 {
+      color: var(--color-primary-2);
+      margin: 0;
+  }
+
+  section {
+      margin-bottom: 80px;
+  }
+</style>
