cce7082bca783d8adccf1bc3ae0e655995f146c7
Pablo Berganza <pberganza10@gmail.com>
2020-04-17 18:27:45 -0600

    :sparkles: Add image optimization script

---
+++ bin/optimg.js
@@ -0,0 +1,55 @@
+const Jimp = require('jimp');
+const path = require('path');
+const globby = require('globby');
+
+function getPaths(imgPath, outputExt = 'processed') {
+  const { dir, ext, name } = path.parse(imgPath);
+  return {
+    input: path.resolve(imgPath),
+    output: path.resolve(
+      path.join(dir, `${name}-${outputExt}${ext}`),
+    ),
+  }
+}
+
+async function generatePlaceholder(imgPath) {
+  const { input, output } = getPaths(imgPath, 'placeholder');
+  const image = await Jimp.read(input);
+  console.log('Processing', input);
+  await image.resize(200, Jimp.AUTO);
+  await image.quality(70);
+  await image.blur(10);
+  console.log('Writing', output, 'to', output);
+  await image.writeAsync(output);
+}
+
+async function generateResized(imgPath, width) {
+  const { input, output } = getPaths(imgPath, width);
+  const image = await Jimp.read(input);
+  console.log('Processing', input);
+  await image.resize(width, Jimp.AUTO);
+  await image.quality(70);
+  console.log('Writing', output, 'to', output);
+  await image.writeAsync(output);
+}
+
+async function generateImages(images) {
+  console.log('#### GENERATING IMAGES ####');
+  return Promise.all(
+    images.map(async imgPath => {
+      await Promise.all([
+        generatePlaceholder(imgPath),
+        generateResized(imgPath, 400),
+        generateResized(imgPath, 800),
+        generateResized(imgPath, 1200),
+        generateResized(imgPath, 1600),
+      ]);
+    }),
+  );
+}
+
+const imgPaths = process.argv.slice(2).flatMap(arg => {
+  return globby.sync(arg);
+});
+
+generateImages(imgPaths);
