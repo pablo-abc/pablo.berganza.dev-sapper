36d1aba9937782326064e5ad265bbd43e0b9539c
Pablo Berganza <pberganza10@gmail.com>
2020-04-17 22:10:30 -0600

    :sparkles: Add 404 page as _error

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,45 @@
 <script>
-	export let status;
-	export let error;
+  import NotFound from './404.svelte';
+  export let status;
+  export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === 'development';
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
+  h1, p {
+      margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+      font-size: 2.8em;
+      font-weight: 700;
+      margin: 0 0 0.5em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+      margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+      h1 {
+	  font-size: 4em;
+      }
+  }
 </style>
 
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+{#if status !== 404}
+  <h1>{status}</h1>
 
-<p>{error.message}</p>
+  <p>{error.message}</p>
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
+{:else}
+  <NotFound />
 {/if}
