0a6129376d96eabeffbf87117b0ee4ac5c8210a8
Pablo Berganza <pberganza10@gmail.com>
2020-11-09 09:04:10 -0600

    :sparkles: add more abilities

--- src/components/AbilityBox.svelte
+++ src/components/AbilityBox.svelte
@@ -2,45 +2,45 @@
   export let src
   export let alt
   export let color
+  export let title
 </script>
-
-<section class="ability color-{color}">
-  <img {src} {alt}>
-</section>
 
 <style>
   .ability {
-      width: 80vw;
-      height: 80vw;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      margin: 0 10vw 10vw 10vw;
-      border-radius: 10px;
+    width: 80vw;
+    height: 80vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 10vw 10vw 10vw;
+    border-radius: 10px;
   }
 
   .ability img {
-      width: 80%;
+    width: 80%;
   }
 
   @media only screen and (min-width: 600px) {
-      .ability {
-          max-width: 250px;
-          max-height: 250px;
-          margin: 24px 16px;
-      }
+    .ability {
+      max-width: 250px;
+      max-height: 250px;
+      margin: 24px 16px;
+    }
   }
 
   .color-1 {
-      background-color: var(--color-secondary-2-1);
+    background-color: var(--color-secondary-2-1);
   }
 
   .color-2 {
-      background-color: var(--color-primary-1);
+    background-color: var(--color-primary-1);
   }
 
   .color-3 {
-      background-color: var(--color-primary-1);
+    background-color: var(--color-primary-1);
   }
-
 </style>
+
+<section class="ability color-{color}">
+  <img {src} {alt} {title} />
+</section>
--- src/components/AbilityBoxes.svelte
+++ src/components/AbilityBoxes.svelte
@@ -7,33 +7,37 @@
     ['img/cljs-white.svg', 'ClojureScript'],
     ['https://cdn.svgporn.com/logos/python.svg', 'Python'],
     ['https://cdn.svgporn.com/logos/docker.svg', 'Docker'],
+    ['img/nestjs.svg', 'NestJS'],
+    ['img/react.svg', 'React'],
+    ['img/svelte.svg', 'Svelte'],
   ]
 </script>
 
-<section class=box-abilities>
-  {#each abilities as ability, index}
-    <AbilityBox
-      color={(index + 1) % 2 || 2}
-      src={ability[0]}
-      alt={ability[1]}
-      />
-  {/each}
-</section>
-
 <style>
   .box-abilities {
-      display: flex;
-      flex-direction: column;
-      width: 100vw;
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
   }
 
   @media only screen and (min-width: 600px) {
-      .box-abilities {
-          flex-direction: row;
-          justify-content: space-around;
-          flex-wrap: wrap;
-          max-width: 960px;
-          margin: 0 auto;
-      }
+    .box-abilities {
+      flex-direction: row;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      max-width: 960px;
+      margin: 0 auto;
+    }
   }
 </style>
+
+<section class="box-abilities">
+  {#each abilities as ability, index}
+    <AbilityBox
+      color="{(index + 1) % 2 || 2}"
+      src="{ability[0]}"
+      alt="{ability[1]}"
+      title="{ability[1]}"
+    />
+  {/each}
+</section>
